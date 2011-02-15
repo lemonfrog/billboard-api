e72d77bbf07683c68f9cf1c46e35736df3ebb687
J. Pablo Fernandez <pablo.fernandez@simplificator.com>
2011-02-15 18:28:25 +0100

    Simplify the configuration a lot.

--- lib/billboard-api/config.rb
+++ lib/billboard-api/config.rb
@@ -1,115 +1,29 @@
 module BillboardApi
-
-  #
-  # Following Settings are required:
-  #  * return_after_payment_url
-  #  * paypal_service_url
-  #  * paypal_notify_url
-  #  * paypal_receiver_email
-  #  * site_url
-
-
-  #
-  # Config is a singleton instance which can be used in two ways:
-  # Config.instance.setting_name1 = 12
-  # Config.instance.setting_name2 = 24
-  # or the (shorter, depending on amount of settings) form with a block
-  # Config.configure do |config|
-  #  config.setting_name1= 12
-  #  config.setting_name2= 24
-  # end 
-  #
-  #
-  # the accessors have possibility to override
-  # settings. This is useful for custom settings on a lower level
-  #
-  # config.setting_name1(12)
-  # => will return 12 all the time which does not make much sense...
-  # config.setting_name(options[:override_setting_name1]) will return the not-nil value from options has
-  # or the setting stored in config.
-  #
   class Config
-    attr_accessor :paypal_service_url
-    attr_reader :url
+    include Singleton
 
-    def self.production?
+    attr_reader :url
+    attr_accessor :paypal_service_url
+
+    def initialize
+      if production?
+        self.url = "https://simplebillboard.com"
+        self.paypal_service_url = "https://www.paypal.com/cgi-bin/webscr?"
+      else
+        self.url = "https://simplebillboard.com.simplificator.com"
+        self.paypal_service_url = "https://www.sandbox.paypal.com/cgi-bin/webscr?"
+      end
+    end
+
+    def url=(value)
+      RemoteResource.site = value
+      @url = value
+    end
+
+    private
+
+    def production?
       !(defined? Rails) || Rails.env.production?
     end
-
-    def self.default
-      if !defined? @@config
-        @@config = Config.new
-
-        if production?
-          @@config.url = "https://simplebillboard.com"
-          @@config.paypal_service_url = "https://www.paypal.com/cgi-bin/webscr?"
-        else
-          @@config.url = "https://simplebillboard.com.simplificator.com"
-          @@config.paypal_service_url = "https://www.sandbox.paypal.com/cgi-bin/webscr?"
-        end
-        puts "Generated default config"
-      end
-      return @@config
-    end
-
-    # When setting a new URL, also change it for the resources that are already loaded.
-    def url=(value)
-      RemoteResource.site= value
-    end
-
-    def self.instance
-      raise "BillboardApi::Config.instance is deprecated"
-    end
-
-#    include Singleton
-
-#    REQUIRED_SETTINGS = [:return_after_payment_url, :paypal_service_url, :paypal_receiver_email]
-#
-#    # Overwrite site urls since they are directly applied to the
-#    # Order class' class variable 'site'
-#    def site_url(override = nil)
-#      override || RemoteResource.site
-#    end
-#    def site_url=(value)
-#      RemoteResource.site= value
-#    end
-#
-#    # Allow access for arbitrary settings
-#    def method_missing(m, *args)
-#      if !m.to_s.ends_with?('=') && args.length <= 1
-#        # return override or setting
-#        args.first || settings[m.to_s]
-#      elsif m.to_s.ends_with?('=') && args.length == 1
-#        settings[m.to_s[0...-1]] = args.first
-#      else
-#        super(m, args)
-#      end
-#    end
-#
-#    # Helper method to ensure all settings are present.
-#    # Will raise a StandardError if Config is invalid.
-#    def validate!
-#      REQUIRED_SETTINGS.each do |name|
-#        raise "Settings '#{name}' is required. Use 'Config.instance.#{name}= VALUE' to set a value" unless self.send(name)
-#      end
-#    end
-#
-#    def clear!
-#      @settings = {}
-#      site_url = nil
-#      nil
-#    end
-#
-#    # Configure with a block
-#    # will yield the instance of Config to the block.
-#    def self.configure(&block)
-#      config = Config.instance
-#      yield(instance) if block_given?
-#    end
-#
-#    private
-#    def settings
-#      @settings ||= {}
-#    end
   end
 end
--- lib/billboard-api/remote_resource.rb
+++ lib/billboard-api/remote_resource.rb
@@ -1,6 +1,6 @@
 module BillboardApi
   # Common superclass to all ActiveResources in Billboard.
   class RemoteResource < ActiveResource::Base
-    self.site = BillboardApi::Config.default.url
+    self.site = BillboardApi::Config.instance.url
   end
 end
