d0a02a6a3425a745c88adbf46a99f601abfdd81c
Tino Wendland <tinowendland@gmail.com>
2012-08-28 13:02:49 +0200

    Setting request format manually to xml because the new default for Rails 3.1 and higher is json, but we need xml.

--- lib/billboard-api/remote_resource.rb
+++ lib/billboard-api/remote_resource.rb
@@ -2,5 +2,8 @@ module BillboardApi
   # Common superclass to all ActiveResources in Billboard.
   class RemoteResource < ActiveResource::Base
     self.site = BillboardApi::Config.instance.url
+    #For Rails >= 3.1 the default request format has been changed from xml to json
+    #Setting it manually ensures that billboard is compatible with all Rails versions in client
+    self.format = :xml
   end
 end
